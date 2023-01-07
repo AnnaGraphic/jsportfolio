5160d793953ccfd698cbc802859822d38c102f88
annar <p4nd4@posteo.de>
2023-01-07 17:39:03 +0100

    add request handler logic

--- main.js
+++ main.js
@@ -28,19 +28,51 @@ const reqPath = path.join(__dirname, "projects", req.url);
     if (req.url == ('/')) {
 
         res.setHeader("Content-Type", "text/html");
-        res.write("<html>");
-        res.write("<body>");
-        res.write("<title>Portfolio</title>");
-        //res.write("link rel='stylesheet' type='text/css' href='styles.css' />");
-        res.write("</body>");
-        res.write("</html>");
+        res.write( `<!doctype html>
+        <html>
+        <title>Portfolio</title>
+        <link rel="stylesheet" href="styles.css" />
+        <h1>Portfolio</h1>
+        </html>`);
         res.statusCode = 200;
-
         res.end();
         return;
     }
 
+if (fs.existsSync(reqPath)) {
+    if (fs.statSync(reqPath).isDirectory()) {
+        if (req.url.endsWith('/')) {
+            const indexHTMLPath = payh.join(reqPath, 'index.html');
+            if (fs.existsSync(indexHTMLPath) && fs.statSync(indexHTMLPath).isFile())
+            {
+                const readStream = fs.createReadStream(indexHTMLPath);
+                readStream.pipe(res);
+                readStream.on('end', () => {
+                    res.end();
+                });
+            } else {
+                res.statusCode = 404;
+                res.end();
+                return;
+            }
+        } else {
+            res.writeHead(301, { Location: req.url + "/"});
+            res.end();
+        }
+    } else {
+        const readStream = fs.createReadStream(reqPath);
+        readStream.pipe(res);
+        readStream.on('end', () => {
+            res.end();
+        });
+    }
+} else {
+    res.statusCode = 404;
+    res.end();
+    return;
+}
 
     console.log("req url: ", req.url);
     console.log("reqPath:  ", reqPath);
+    
 }).listen(PORT, () => console.log(`Listening on port: ${PORT}`))
